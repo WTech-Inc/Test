0d133b8f4c21bbf5b24ecfe3561d670d4fb835b6
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-03-06 09:01:18 +0800

    Create wchat.py

---
+++ wbank/wchat.py
@@ -0,0 +1,16 @@
+from j import complie
+
+jCode = """
+  // prints : print string
+  // printIn : print Integer
+  // printf : print with float
+  public static void start() {
+    for ( Integer i = 0 ; i ==5 ; System.output.printIn(i)) {
+      i += 1;
+    }
+    System.output.prints("Loop complete");
+  }
+"""
+
+print(complie(jCode, target="py"))
+# Then it will transfer to py lang
