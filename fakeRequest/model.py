9acea9d22d52ba6073d2220f14d401a866f50db8
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-03-08 13:47:58 +0800

    Update model.py

--- fakeRequest/model.py
+++ fakeRequest/model.py
@@ -1,13 +1,34 @@
 
+supportLang = ["py","nodeJS"]
+pyWords = {
+  "print":"print()",
+  "condition":["if","elif",else"]
+}
 
 class Brain:
-    def __init__(self):
-        pass
+    def __init__(self,prompt):
+        self.input = prompt
+        self.coding = False
+        self.output = ""
+        return self.inputLayer()
     def inputLayer(self):
-        pass
+        self.prompt = self.input.strip()
+        return self.thinkingLayer()
     def thinkingLayer(self):
-        pass
-    def deepThinkLayer(self):
-        pass
+        if "Code" in self.prompt:
+            self.coding = True
+        if self.coding:
+            for lang in supportLang:
+                if lang in self.prompt:
+                    if lang == "py":
+                        return self.deepThinkLayer(lang)
+                    elif lang == "nodeJS":
+                        return self.deepThinkLayer(lang)
+                    
+    def deepThinkLayer(self, lang):
+        if lang == "py":
+            pass
+        elif lang == "nodeJS":
+            pass
     def outputLayer(self):
         pass
