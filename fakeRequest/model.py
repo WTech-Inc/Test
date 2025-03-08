d41d9bf5e80ef851db8350a5dfb0044ba4e33e3e
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-03-08 14:21:26 +0800

    Update model.py

--- fakeRequest/model.py
+++ fakeRequest/model.py
@@ -1,16 +1,15 @@
 
 supportLang = ["py","nodeJS"]
 pyWords = {
-  "print":"print()",
-  "symbols":["=","==",">=","()","!="],
+  "print":"print(.)",
+  "symbols":["=","==",">=","(.)","!="],
   "dataType":["str","int","float","bool"],
   "condition":["if","elif","else"],
   "loop":["while","for","in","range"],
   "error":["try","except"],
   "class":["class"],
   "functions":["def","@"],
-  "import":["from","import"],
-  "webModules":["flask"]
+  "import":["from","import"]
 }
 
 class Brain:
@@ -35,16 +34,23 @@ class Brain:
                     
     def deepThinkLayer(self, lang):
         if lang == "py":
-          if "web" in self.prompt:
+          if 'print' in self.prompt:
             self.output = f'''
-            {pyWords["import"][0]} {pyWords["webModules"][0]} {pyWords["import"][1]} *
-            app {pyWords["symbols"][0]} Flask("app")
-            {pyWords["functions"][1]}app.route("/")
-            {pyWords["functions"][0]} index{pyWords["symbols"][3]}:
-              return "Hi"
-              
-            app.run()
+            {pyWords["print"].split(".")[0]}放你想打印的東西{pyWords["print"].split(".")[1]}
             '''
+          elif 'for' in self.prompt:
+            self.output = f'''
+            # range() 函數：
+            # return a list for 用戶輸入
+            # 參數：
+            # 1: start -- 由什麼數值開始
+            # 2: end -- 什麼數值停止（一定要end的數值+1。例子：如果我想在10停止，就要end=11。)
+            # 3: step (可選參數)-- 步長 （即你想每一個數值中間隔距如何，預設是1)
+            {pyWords["loop"][1]} i {pyWords["loop"][2]} {pyWords["loop"][3]}{pyWords["symbols"][3].split(".")[0]}0,11,1{pyWords["symbols"][3].split(".")[1]}:
+              {pyWords["print"].split(".")[0]}i{pyWords["print"].split(".")[1]}
+            '''
+          elif '函數' in self.prompt:
+            pass
             return self.outputLayer()
         elif lang == "nodeJS":
           pass
