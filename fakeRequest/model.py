6c6a46b6d4006a73f72639c3f23557a1aea997b9
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-03-08 13:34:29 +0800

    One brain four layer

--- fakeRequest/model.py
+++ fakeRequest/model.py
@@ -1,1 +1,13 @@
 
+
+class Brain:
+    def __init__(self):
+        pass
+    def inputLayer(self):
+        pass
+    def thinkingLayer(self):
+        pass
+    def deepThinkLayer(self):
+        pass
+    def outputLayer(self):
+        pass
