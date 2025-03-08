823a49c2513e13305649d5518c3733a799bd3350
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-03-08 14:02:56 +0800

    Coding brain

--- fakeRequest/model.py
+++ fakeRequest/model.py
@@ -2,12 +2,14 @@
 supportLang = ["py","nodeJS"]
 pyWords = {
   "print":"print()",
+  "symbols":["=","==",">=","()","!="],
+  "dataType":["str","int","float","bool"],
   "condition":["if","elif","else"],
   "loop":["while","for","in","range"],
   "error":["try","except"],
   "class":["class"],
   "functions":["def","@"],
-  "import":["from","import"]
+  "import":["from","import"],
   "webModules":["flask"]
 }
 
@@ -34,12 +36,17 @@ class Brain:
     def deepThinkLayer(self, lang):
         if lang == "py":
           if "web" in self.prompt:
-            self.output = f"""
+            self.output = f'''
             {pyWords["import"][0]} {pyWords["webModules"][0]} {pyWords["import"][1]} *
-            app = Flask("app")
-            
-            """
+            app {pyWords["symbols"][0]} Flask("app")
+            {pyWords["functions"][1]}app.route("/")
+            {pyWords["functions"][0]} index{pyWords["symbols"][3]}:
+              return "Hi"
+              
+            app.run()
+            '''
+            return self.outputLayer()
         elif lang == "nodeJS":
           pass
     def outputLayer(self):
-        pass
+        print(self.output)
