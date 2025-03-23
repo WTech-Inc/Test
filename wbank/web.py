32d026ba91c256b4bac4ef9233b9ce1bfa3cc801
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-03-23 11:40:13 +0800

    Update web.py (get request method)

--- wbank/web.py
+++ wbank/web.py
@@ -1,5 +1,5 @@
 from wnet import App
-from wnet.https import Controller, Response, jsonify
+from wnet.https import Controller, Response, jsonify, render
 
 app = App(apiKey="key", httpsSSL=True)
 con = Controller(app, host="127.0.0.1", port=5000, usingDatabase=True)
@@ -11,6 +11,8 @@ def index(req):
 
 @con.route("/login")
 def login(req):
+    if req.method == "GET":
+        return render("index.html")
     username = req.form.get("username")
     pw = req.form.get("pw")
     users = con.db.session.query("select * from users where username=(%s)", (username)).fetchone()
