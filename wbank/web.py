13b40fd69840fa8dbbcf4a34c0c5feceffba2af5
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-03-23 11:36:56 +0800

    Update web.py

--- wbank/web.py
+++ wbank/web.py
@@ -1,12 +1,24 @@
 from wnet import App
-from wnet.https import Controller, Response
+from wnet.https import Controller, Response, jsonify
 
 app = App(apiKey="key", httpsSSL=True)
-con = Controller(app, host="127.0.0.1", port=5000)
+con = Controller(app, host="127.0.0.1", port=5000, usingDatabase=True)
+con.config("dataurl","psql-url")
 
 @con.route("/")
 def index(req):
     return Response("Hello", code=200)
+
+@con.route("/login")
+def login(req):
+    username = req.form.get("username")
+    pw = req.form.get("pw")
+    users = con.db.session.query("select * from users where username=(%s)", (username)).fetchone()
+    if users:
+        if pw == users["password"]:
+            return Response("Login success", code=200)
+    return Response(jsonify({"msg":"Users not found"}), code=404)
+    
 
 app.add_threads([con])
 app.run()
