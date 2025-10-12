f99698fd29a595cd2baa26bc2f6e362169117861
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-10-12 22:01:26 +0800

    Update tryweb.py

--- tryweb.py
+++ tryweb.py
@@ -3,10 +3,13 @@ from tryweb.apps import basicServer
 from tryweb.views import Views
 from tryweb.pages import FontManager
 from tryweb.pages.colors import rgb
+from tryweb.complier import tryCode
+from trydb import DataBase
 
 server = basicServer()
 con = Controller(server=server)
 views = Views(con)
+db = con.use("trydb", DataBase("sqlite:///users.db"))
 
 class index_page_view(views):
     __viewname__ = "index page"
@@ -17,10 +20,12 @@ class index_page_view(views):
     @body
     def _body(writer):
         writer(FontManager(type="h2", text="Hello", color=rgb(0,0,0)))
-    
+        writer(tryCode=tryCode("using web.front -> nextLine", con))
+        writer(tryCode=tryCode("using web.front -> new-font='passage-text',color='RED' -> value='你好，這是html p tag的文字'", con))
+        writer(tryCode=tryCode("using web.front -> nextLine", con))
 
 @con.on_route("/")
 def index(req, res):
-    return views.find(name="index page")
+    return res.reply(view=views.find(name="index page"))
 
 server.run()
