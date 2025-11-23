ded73274464c5210da4112a3f3026f9bc598c736
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-11-23 20:50:53 +0800

    Create tryweb.py

---
+++ fakeRequest/try.py
@@ -0,0 +1,11 @@
+import tryweb
+
+web = tryweb.Connection()
+route = tryweb.Router(web)
+web.webs.use("@+tryweb/front")
+
+@route.getMapping("/")
+def index(req, res):
+    return res.reply_with_html("pages/index.html, text="hello")
+
+web.start_listen(port=5000)
