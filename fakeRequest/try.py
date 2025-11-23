b89f576f6ce3479c1fcc5aea34fb2d1c14dc7f8c
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-11-23 20:53:00 +0800

    Update try.py

--- fakeRequest/try.py
+++ fakeRequest/try.py
@@ -6,6 +6,6 @@ web.webs.use("@+tryweb/front")
 
 @route.getMapping("/")
 def index(req, res):
-    return res.reply_with_html("pages/index.html, text="hello")
+    return res.reply_with_html("pages/index.html", text="hello")
 
 web.start_listen(port=5000)
