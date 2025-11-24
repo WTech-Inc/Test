dc1043ce7e1dad98a45805139f4e1e192f194cff
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-11-24 11:09:44 +0800

    Update try.py

--- fakeRequest/try.py
+++ fakeRequest/try.py
@@ -9,3 +9,20 @@ def index(req, res):
     return res.reply_with_html("pages/index.html", text="hello")
 
 web.start_listen(port=5000)
+
+# index.html
+"""
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>測試頁面 -- index.html</title>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+     {% using tryweb.front -> pageCenter %}
+     {% using tryweb.front -> nextLine %}
+     {% using tryweb.front -> nextLine %}
+     The text is : {% call tryweb.page_var("text") %}
+  </body>
+</html>
+"""
