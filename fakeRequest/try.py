b563e65418c52e8a918dd9da827afe5802f567b8
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-12-02 21:10:21 +0800

    Update try.py

--- fakeRequest/try.py
+++ fakeRequest/try.py
@@ -3,10 +3,28 @@ import tryweb
 web = tryweb.Connection()
 route = tryweb.Router(web)
 web.webs.use("@+tryweb/front")
+web.webs.use("@+tryweb/db")
+
+db = tryweb.DataBase(url="sqlite:///test.db")
 
 @route.getMapping("/")
 def index(req, res):
     return res.reply_with_html("pages/index.html", text="hello")
+
+@route.GetMapping("/get/products")
+def get_products(req, res):
+  results, err = db.sql.execute("select * from product")
+  if err or err != None: return res.reply_with_text(err.toMessage())
+  for result in results:
+    name = result["name"]
+    price = result["price"]
+    return res.reply_with_xml(f"""
+    <?xml version='1.0' encoding='UTF-8'>
+      <products>
+        <product name="{name}" price="{price}" />
+      </products>
+    </?xml>
+  """)
 
 web.start_listen(port=5000)
 
